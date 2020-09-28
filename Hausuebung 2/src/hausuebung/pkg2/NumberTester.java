edfe394318528f6cced064debe22665acba5a1c5
Florian <floriankaufmann@gmx.at>
2020-09-28 15:52:44 +0200

    3 Beispiel Ende der Einheit

--- Hausuebung 2/src/hausuebung/pkg2/NumberTester.java
+++ Hausuebung 2/src/hausuebung/pkg2/NumberTester.java
@@ -5,31 +5,58 @@
  */
 package hausuebung.pkg2;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 /**
  *
  * @author flori
  */
 public class NumberTester {
-    String fileName;
+    BufferedReader reader;
+    NumberTest oddTester;
+    NumberTest primeTester;
+    NumberTest palindromeTester;
     
     public NumberTester(String fileName) {
-        this.fileName = fileName;
+        try {
+            reader = new BufferedReader(new FileReader(fileName));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(NumberTester.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
-    public void setOddEvenTester(NumberTest oddTest){
-        
+    public void setOddEvenTester(NumberTest oddTester){
+        this.oddTester = oddTester;
     }
     
     public void setPrimeTest(NumberTest primeTester){
-    
+        this.primeTester = primeTester;
     }
     
     public void setPalindromeTester(NumberTest palindromeTester){
-    
+        this.palindromeTester = palindromeTester;
     }
     
     public void testFile(){
-    
+        String left;
+        String right;
+        try {
+            String line = reader.readLine();
+            while(reader.ready()){
+                line = reader.readLine();
+            };
+        } catch (IOException ex) {
+            Logger.getLogger(NumberTester.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            
+        
     }
     
 }
