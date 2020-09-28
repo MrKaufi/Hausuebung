84ed367a7aba361f8cf0ded8471252e2da070e13
Florian <floriankaufmann@gmx.at>
2020-09-28 14:38:55 +0200

    2 Beispiel finished

---
+++ Hausuebung 2/src/hausuebung/pkg2/HalloJavamitForEach.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hausuebung.pkg2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author flori
+ */
+public class HalloJavamitForEach {
+    
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+
+        list.add("Hello");
+        list.add("There");
+        list.add("OooOOooOOoo");
+        list.add("OooOOoOOoo");
+        list.add("OooOOooOOooooo");
+        list.forEach((String s) -> System.out.println(s));
+    }   
+}
