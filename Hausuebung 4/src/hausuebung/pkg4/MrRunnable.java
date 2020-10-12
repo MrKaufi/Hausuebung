5090edf80152a50c55bbdbed21022a57dcbc26de
Florian <floriankaufmann@gmx.at>
2020-10-12 15:53:08 +0200

    Ende Einheit

---
+++ Hausuebung 4/src/hausuebung/pkg4/MrRunnable.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hausuebung.pkg4;
+
+/**
+ *
+ * @author flori
+ */
+public class MrRunnable implements Runnable{
+    int teiler;
+
+    public MrRunnable(int teiler) {
+        this.teiler = teiler;
+    }
+    
+    
+    
+    
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
