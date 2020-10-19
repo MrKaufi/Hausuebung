91f2d3f66eb48effc7c371c11846961233b1b33b
Florian <floriankaufmann@gmx.at>
2020-10-19 11:15:29 +0200

    19.10

---
+++ Sudoku_StudentStub/Sudoku_StudentStub/src/net/htlgrieskirchen/pos3/sudoku/SudokuSolver.java
@@ -0,0 +1,55 @@
+package net.htlgrieskirchen.pos3.sudoku;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/* Please enter here an answer to task four between the tags:
+ * <answerTask4>
+ *    Hier sollte die Antwort auf die Aufgabe 4 stehen.
+ * </answerTask4>
+ */
+public class SudokuSolver implements ISodukoSolver {    
+    
+    public SudokuSolver() {
+        //initialize if necessary
+    }
+
+    @Override
+    public final int[][] readSudoku(File file) {
+        int[][] sudoku;
+        
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(SudokuSolver.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        // implement this method
+        return new int[0][0]; // delete this line!
+    }
+
+    @Override
+    public boolean checkSudoku(int[][] rawSudoku) {
+        // implement this method
+        return false; // delete this line!
+    }
+
+    @Override
+    public int[][] solveSudoku(int[][] rawSudoku) {
+        // implement this method
+        return new int[0][0]; // delete this line!
+    }
+    
+    @Override
+    public int[][] solveSudokuParallel(int[][] rawSudoku) {
+        // implement this method
+        return new int[0][0]; // delete this line!
+    }
+
+    // add helper methods here if necessary
+}
